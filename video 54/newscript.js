fb092e46ada7b7a5ca03f201bf0846e6afbb4731
Amaresh Sahoo <eb.amareshsahoo@gmail.com>
2023-12-01 13:27:56 +0530

    video 54

---
+++ video 54/newscript.js
@@ -0,0 +1,1 @@
+console.log("cosole is running..")
---
+++ video 54/script.js
@@ -0,0 +1,21 @@
+alert("Hello World");
+
+console.log("cosole is running..")
+
+var a = prompt("Enter your number")
+
+console.log("your number is "+ a)
+
+var isTrue = confirm("Are you shure u want to leave this page and blast your computer? ")
+
+if(isTrue){
+    console.log("your computer is blasting")
+}
+else {
+    console.log("your computer is not blasting")
+}
+
+
+document.title = "Hey i am good"
+
+document.body.style.backgroundColor = "red"
