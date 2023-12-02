f18b62ae1fb8050d7b166c1a95805ef010911891
Amaresh Sahoo <eb.amareshsahoo@gmail.com>
2023-12-02 12:13:11 +0530

    video 55

---
+++ video 55/script.js
@@ -0,0 +1,44 @@
+console.log("this is video 55 of SWDC.")
+
+let a = 5;
+
+a = a+ 1;
+let b = 6;
+let c = "Harry"
+// var 55a = "hammer"
+
+console.log(a + b + 8);
+
+console.log(typeof a , typeof b , typeof c);
+
+// const a1 = 6;
+//  a1 = a1 + 1;
+
+ // Not allowed
+
+
+{
+    let a = 66;
+    console.log(a)
+}
+
+let x = "Amaresh Sahoo";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(typeof x ,typeof y ,typeof z ,typeof p ,typeof q ,typeof r)
+
+let o = {
+    name : "Amaresh",
+    "job code" : 5600,
+    "is handsome" : true ,   
+};
+
+o.salary = "100crore";
+
+console.log(o);
+
+console.log(o["job code"]);
