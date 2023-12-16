e14c3421d5e2b130e8fb2fd0e7bd87981c0bd21b
Amaresh Sahoo <eb.amareshsahoo@gmail.com>
2023-12-16 23:13:38 +0530

    message

---
+++ video 68/script.js
@@ -0,0 +1,22 @@
+console.log("Amaresh")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById("redBox").style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColor = "green"
+
+// console.log(document.querySelectorAll(".box"))
+
+// document.querySelectorAll(".box").forEach(e =>{
+//     e.style.backgroundColor = "green";
+// })
+
+e = document.getElementsByTagName("div")
+
+
+
+console.log(e[3].matches("redBox"))
